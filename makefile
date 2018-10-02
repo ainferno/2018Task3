1eb618720639bc1a490758911684784cb0bc60d3
Ainferno <vasily.nichiporenko@ya.ru>
2018-10-02 17:27:09 +0300

    buff_finished

---
+++ makefile
@@ -0,0 +1,2 @@
+all:
+	gcc task3.c str_list.c buffer.c
--- task3.c
+++ task3.c
@@ -3,11 +3,57 @@
 #include "str_list.h"
 #include "buffer.h"
 
+void input()
+{
+    char *str = NULL;
+    char c;
+    int N = 10, i = 0;
+    str = (char*)malloc(N*sizeof(char));
+
+    for(;(c = get_char()) != '\0';i++)
+    {
+        if(i == N)
+        {
+            N*=2;
+            str = (char*)realloc(str, N*sizeof(char));
+        }
+        if( i > 0)
+        {
+            switch(c)
+            {
+                case ' ': case '\n': case '\t':
+                    N = 10;
+                    str[i] = '\0';
+        //            add(&a, str);
+                    str = (char*)malloc(N*sizeof(char));
+                    i = -1;
+            }
+        }
+        if(c != ' ' && c != '\n' && c != '\t')
+        {
+            str[i] = c;
+        }
+        else
+            i--;
+    }
+}
+
 int main()
 {
-    char c, b[10] = "qwerty";
-    
-    while((c = get_c()) != '\0')
-        printf("%c", c);
+    string_list lst = (char**)malloc(10 * sizeof(char*));
+    add_string_list(lst, "aaaa");
+    add_string_list(lst, "bbbb");
+    add_string_list(lst, "cccc");
+    add_string_list(lst, "dddd");
+    // print_string_list(lst);
+    for(int i = 0;i < 2;i++)
+        printf("%s\n", lst[3]);
+
+    // free(lst[0]);
+    // free(lst[1]);
+    // free(lst[2]);
+    // free(lst[3]);
+    // free(lst);
+    //input();
     return 0;
 }
